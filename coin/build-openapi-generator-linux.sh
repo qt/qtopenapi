ee1080a8b652991c822cd45ada6207430f88fe67
Tatiana Borisova <tatiana.borisova@qt.io>
2025-08-18 12:57:36 +0200

    Add provisioning step to the qtopenapi project
    Related to 33dd0de8fb51bb83230b8c2524d5aae28ffc799b
    
    Task-number: QTBUG-138144
    Change-Id: I7d344dfa7ba01552d34f4faf73100f85f6070eea
    Reviewed-by: Simo Fält <simo.falt@qt.io>

--- coin/build-openapi-generator-linux.sh
+++ coin/build-openapi-generator-linux.sh
@@ -9,18 +9,11 @@ sudo apt-get -y install maven
 OPENAPI_HOME=/home/qt/work/playground/qtopenapi
 JAVA_HOME=$(dirname $(dirname $(readlink -e /usr/bin/javac)))
 cd $OPENAPI_HOME
-mkdir -p $OPENAPI_HOME/openapi_client_generators
-curl https://raw.githubusercontent.com/OpenAPITools/openapi-generator/master/bin/utils/openapi-generator-cli.sh -o $OPENAPI_HOME/openapi_client_generators/openapi-generator-cli
-chmod u+x $OPENAPI_HOME/openapi_client_generators/openapi-generator-cli
 
-#### Here version should be updated manually
-curl https://repo1.maven.org/maven2/org/openapitools/openapi-generator-cli/7.12.0/openapi-generator-cli-7.12.0.jar -o $OPENAPI_HOME/openapi_client_generators/openapi-generator-cli-7.12.0.jar
-
-PATH=$PATH:$OPENAPI_HOME/openapi_client_generators
 mvn clean
 mvn package
 
-OPENAPI_CLI="openapi_client_generators/openapi-generator-cli-7.12.0.jar"
+OPENAPI_CLI="/opt/qt-openapi/openapi-generator-cli.jar"
 OPENAPI_CLI_ENTRYPOINT_CLASS="org.openapitools.codegen.OpenAPIGenerator"
 ORIGINAL_GENERATOR="cpp-qt6-client"
 ORIGINAL_GENERATOR_JAR="$OPENAPI_HOME/target/cpp-qt6-client-openapi-generator-1.0.0.jar"
--- coin/build-openapi-generator-macos.sh
+++ coin/build-openapi-generator-macos.sh
@@ -8,18 +8,11 @@ brew install maven
 
 OPENAPI_HOME=/Users/qt/work/playground/qtopenapi
 cd $OPENAPI_HOME
-mkdir -p $OPENAPI_HOME/openapi_client_generators
-curl https://raw.githubusercontent.com/OpenAPITools/openapi-generator/master/bin/utils/openapi-generator-cli.sh -o $OPENAPI_HOME/openapi_client_generators/openapi-generator-cli
-chmod u+x $OPENAPI_HOME/openapi_client_generators/openapi-generator-cli
 
-#### Here version should be updated manually
-curl https://repo1.maven.org/maven2/org/openapitools/openapi-generator-cli/7.12.0/openapi-generator-cli-7.12.0.jar -o $OPENAPI_HOME/openapi_client_generators/openapi-generator-cli-7.12.0.jar
-
-PATH=$PATH:/opt/homebrew/opt/qt@5/bin:$OPENAPI_HOME/openapi_client_generators
 mvn clean
 mvn package
 
-OPENAPI_CLI="openapi_client_generators/openapi-generator-cli-7.12.0.jar"
+OPENAPI_CLI="/opt/qt-openapi/openapi-generator-cli.jar"
 OPENAPI_CLI_ENTRYPOINT_CLASS="org.openapitools.codegen.OpenAPIGenerator"
 ORIGINAL_GENERATOR="cpp-qt6-client"
 ORIGINAL_GENERATOR_JAR="$OPENAPI_HOME/target/cpp-qt6-client-openapi-generator-1.0.0.jar"
