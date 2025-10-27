ff2270f0f138eb3ef3ee26a9eee71bee6c3ce0e2
Ivan Solovev <ivan.solovev@qt.io>
2025-10-27 12:50:50 +0100

    Temporary build OpenAPI library
    QtOpenAPI does not have any Qt-specific code, it only generates
    client libraries from provided templates.
    Because of that, the module was built as an interface library.
    This, however, causes bugs in release-and-debug configuration.
    Specifically, an attempt to configure tests does nothing because
    the QT_FEATURE_openapi_generator feature is not found, and so all
    tests are skipped.
    
    Ideally this should be fixed at the cmake level in qtbase, but as
    a temporary solution simply add a dummy cpp file with a dummy
    exported function.
    Also add missing licenses to LICENSES subdir.
    
    Task-number: QTBUG-138968
    Change-Id: I8641cf394bf1d3e05ed4c6e88e3f8554a4548ce5
    Reviewed-by: Rym Bouabid <rym.bouabid@qt.io>
    Reviewed-by: Alexandru Croitor <alexandru.croitor@qt.io>

---
+++ src/openapi/dummy.cpp
@@ -0,0 +1,9 @@
+// Copyright (C) 2025 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+
+#include <QtOpenapi/qtopenapiexports.h>
+
+Q_OPENAPI_EXPORT void dummyFunction()
+{
+    return;
+}
