a9831bcbacf8c570afef8717b12d1daf2001249e
Rym Bouabid <rym.bouabid@qt.io>
2025-12-17 18:22:16 +0100

    Generate using openapi-generator-cli executable if jar file is not found
    We used to only search for "openapi-generator-cli.jar" file. If not
    found, we would immediately skip building qtopenapi.
    
    Users may install openapi-generator-cli via package managers (npm, pip,
    Homebrew, Scoop). Generation should work like other OpenAPI generators.
    
    In case of npm: Generating using the executable will install the latest
    stable jar version, if the jar file was not found.
    
    Since we are always creating a custom generator, only the npm-installed
    executable supports direct generation via the --custom-generator option,
    with other package manager installations, the jar file must be used
    manually. The challenge here is to find its path as the name can change
    from a version to another and a system to another.
    
    New behavior:
    
    - Search for openapi-generator-cli or openapi-generator files as
    fallback when jar file is not found.
    
    - Check if the installed file supports --custom-generator:
     * npm: Use --custom-generator option directly
    - If it doesn't have it, detect installation method and find the jar
    file accordingly:
      * pip: Exract jar path from the python package openapi-generator-cli
      * TODO: Homebrew (macOS) and Scoop (WIN)
    
    - Set an additional INTERFACE_OPENAPI_GENERATOR_CLI_EXECUTABLE property
    on the WrapOpenAPIGenerator target that holds the executable path.
    Only set it if the executable supports --custom-generator.
    
    - Modify the custom command to use the detected executable when
     1) OPENAPI_GENERATOR_CLI_JAR is not found
        +
     2) OPENAPI_GENERATOR_CLI_EXECUTABLE is set.
    
    - Add a DEBUG message when:
     * Jar file is not found.
     * Executable is also not found.
     * Executable is found but it does not support custom generators and the
       expected jar file path does not exist.
    
    Task-number: QTBUG-142545
    Pick-to: 6.11
    Change-Id: I609de2c8ca3f1f65211a8fcc54d823a6df93b875
    Reviewed-by: Alexandru Croitor <alexandru.croitor@qt.io>
    Reviewed-by: Ivan Solovev <ivan.solovev@qt.io>

--- cmake/FindWrapOpenAPIGenerator.cmake
+++ cmake/FindWrapOpenAPIGenerator.cmake
@@ -1,11 +1,14 @@
 # Copyright (C) 2025 The Qt Company Ltd.
 # SPDX-License-Identifier: BSD-3-Clause
 
+include("FindWrapOpenAPIGeneratorHelpers")
+
 if(TARGET WrapOpenAPIGenerator::WrapOpenAPIGenerator)
     set(WrapOpenAPIGenerator_FOUND TRUE)
     return()
 endif()
 
+# Option 1: Try to find the jar file "openapi-generator-cli.jar"
 # We don't use find_jar, because that passes NO_DEFAULT_PATH to find_path, and we want to search
 # inside PATH env.
 find_file(OPENAPI_GENERATOR_CLI_JAR
@@ -18,16 +21,52 @@ find_file(OPENAPI_GENERATOR_CLI_JAR
         ${Java_JAR_PATHS}
 )
 
+# Option 2: - Try to find openapi-generator-cli executable
+if(NOT OPENAPI_GENERATOR_CLI_JAR)
+    message(DEBUG "Could NOT find openapi-generator-cli.jar")
+
+    find_program(OPENAPI_GENERATOR_CLI_EXECUTABLE NAMES openapi-generator-cli openapi-generator)
+    if(OPENAPI_GENERATOR_CLI_EXECUTABLE)
+        message(DEBUG "Found openapi-generator-cli executable: ${OPENAPI_GENERATOR_CLI_EXECUTABLE}")
+
+        _qt_internal_check_custom_generator_support(__qt_openapi_supports_custom_generator)
+        if(NOT __qt_openapi_supports_custom_generator)
+            _qt_internal_openapi_find_cli_jar(OPENAPI_GENERATOR_CLI_JAR)
+        endif()
+    else()
+        message(DEBUG "Could NOT find openapi-generator-cli executable")
+    endif()
+endif()
+
+if(OPENAPI_GENERATOR_CLI_JAR)
+    set(__qt_openapi_required_vars "OPENAPI_GENERATOR_CLI_JAR")
+elseif(OPENAPI_GENERATOR_CLI_EXECUTABLE AND __qt_openapi_supports_custom_generator)
+    set(__qt_openapi_required_vars "OPENAPI_GENERATOR_CLI_EXECUTABLE")
+else()
+    # Either nothing found, or cli file found but doesn't support custom generators.
+    # In either case, it's not usable for qtopenapi.
+    set(__qt_openapi_required_vars
+        OPENAPI_GENERATOR_CLI_JAR
+        OPENAPI_GENERATOR_CLI_EXECUTABLE)
+endif()
+
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(WrapOpenAPIGenerator
     REQUIRED_VARS
-        OPENAPI_GENERATOR_CLI_JAR
+       ${__qt_openapi_required_vars}
 )
+
+unset(__qt_openapi_required_vars)
 
 if(WrapOpenAPIGenerator_FOUND)
     add_library(WrapOpenAPIGenerator::WrapOpenAPIGenerator INTERFACE IMPORTED)
-    set_target_properties(WrapOpenAPIGenerator::WrapOpenAPIGenerator PROPERTIES
-        INTERFACE_OPENAPI_GENERATOR_CLI_JAR "${OPENAPI_GENERATOR_CLI_JAR}"
-    )
+    if(OPENAPI_GENERATOR_CLI_JAR)
+        set_target_properties(WrapOpenAPIGenerator::WrapOpenAPIGenerator PROPERTIES
+            INTERFACE_OPENAPI_GENERATOR_CLI_JAR "${OPENAPI_GENERATOR_CLI_JAR}"
+        )
+    elseif(OPENAPI_GENERATOR_CLI_EXECUTABLE AND __qt_openapi_supports_custom_generator)
+        set_target_properties(WrapOpenAPIGenerator::WrapOpenAPIGenerator PROPERTIES
+            INTERFACE_OPENAPI_GENERATOR_CLI_EXECUTABLE "${OPENAPI_GENERATOR_CLI_EXECUTABLE}"
+        )
+    endif()
 endif()
-
