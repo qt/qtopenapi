a3f9a71600a237525206a6c20480100af9f09986
Ivan Solovev <ivan.solovev@qt.io>
2026-02-09 17:27:20 +0100

    Do not add a version to the name of the jar file
    By default, maven adds a version to the name of the generated
    jar file. However, we normally do not do that for the binaries
    that we ship as a part of Qt.
    
    This patch adds a special entry to the pom.xml file, which
    instructs maven to not use the version in the name of the
    generated jar file, and also adjusts the relevant CMake
    scripts.
    
    Fixes: QTBUG-143768
    Pick-to: 6.11
    Change-Id: I42acded4468b254b336d0929b2cd2c470c423dbb
    Reviewed-by: Alexandru Croitor <alexandru.croitor@qt.io>
    Reviewed-by: Tatiana Borisova <tatiana.borisova@qt.io>

--- src/openapi/Qt6OpenApiToolsExtraTargets.cmake
+++ src/openapi/Qt6OpenApiToolsExtraTargets.cmake
@@ -5,7 +5,7 @@ if(NOT TARGET "${QT_CMAKE_EXPORT_NAMESPACE}::QtOpenAPIGeneratorJar")
     add_executable("${QT_CMAKE_EXPORT_NAMESPACE}::QtOpenAPIGeneratorJar" IMPORTED GLOBAL)
 
     set(_qt_openapi_generator_path
-        "${QT6_INSTALL_PREFIX}/${QT6_INSTALL_LIBEXECS}/cpp-qt6-client-openapi-generator-${QT_OPENAPI_GENERATOR_VERSION}.jar")
+        "${QT6_INSTALL_PREFIX}/${QT6_INSTALL_LIBEXECS}/cpp-qt6-client-openapi-generator.jar")
 
     set_target_properties("${QT_CMAKE_EXPORT_NAMESPACE}::QtOpenAPIGeneratorJar"
         PROPERTIES IMPORTED_LOCATION "${_qt_openapi_generator_path}")
