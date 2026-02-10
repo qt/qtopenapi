6c308ee8fbb2098e4a370e959c405d9c2468fb82
Rym Bouabid <rym.bouabid@qt.io>
2026-02-10 16:49:25 +0100

    Add GENERATE_OPTIONS argument to qt_add_openapi_client
    OpenAPI Generator provides a large set of generation options:
    https://openapi-generator.tech/docs/usage#generate
    
    Add a GENERATE_OPTIONS multi-value argument to allow passing more
    generator options. This avoids having to extend the macro for
    every new option introduced by the OpenAPI Generator.
    
    Document what should the user expect when using it with the other
    available clashing arguments in a follow-up patch.
    
    Task-number: QTBUG-143166
    Pick-to: 6.11
    Change-Id: I7aed5ab4812002cd69fce96a043c04819f96a790
    Reviewed-by: Ivan Solovev <ivan.solovev@qt.io>

--- tests/auto/compression/client/tst_compression/tst_compression.cpp
+++ tests/auto/compression/client/tst_compression/tst_compression.cpp
@@ -4,7 +4,7 @@
 #include "qoaicommonglobal.h"
 #include "qoaihttprequest.h"
 
-#include "testapi.h"
+#include "compressiontestapi.h"
 
 #include <QtCore/qobject.h>
 #include <QtCore/qprocess.h>
@@ -138,7 +138,7 @@ void tst_Compression::toggleCompressionParameters()
     manager.m_forceCompressedResponse = forceCompressedResponse;
     QRestAccessManager restManager(&manager);
 
-    QtOpenAPI::TestApi api;
+    CompressionNamespace::CompressionTestApi api;
     api.setRestAccessManager(&restManager);
 
     // compression is disabled by default
